c33cb609e8eacd2a7064738cace1f60b6352fdaf
韋廷 呂 <daniel1002135@gmail.com>
2022-10-22 14:45:54 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace RS232ReadandWriteApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.port_printer = new System.IO.Ports.SerialPort(this.components);
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.txtbx_portlist = new System.Windows.Forms.TextBox();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // port_printer
+            // 
+            this.port_printer.BaudRate = 4800;
+            this.port_printer.PortName = "COM3";
+            this.port_printer.ReadTimeout = 500;
+            this.port_printer.WriteTimeout = 500;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(31, 26);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(85, 39);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "連線";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // txtbx_portlist
+            // 
+            this.txtbx_portlist.Location = new System.Drawing.Point(361, 71);
+            this.txtbx_portlist.Multiline = true;
+            this.txtbx_portlist.Name = "txtbx_portlist";
+            this.txtbx_portlist.ReadOnly = true;
+            this.txtbx_portlist.Size = new System.Drawing.Size(183, 173);
+            this.txtbx_portlist.TabIndex = 1;
+            // 
+            // btn_print
+            // 
+            this.btn_print.Location = new System.Drawing.Point(31, 97);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(85, 41);
+            this.btn_print.TabIndex = 2;
+            this.btn_print.Text = "列印";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.txtbx_portlist);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "RS232 Printer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort port_printer;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.TextBox txtbx_portlist;
+        private System.Windows.Forms.Button btn_print;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RS232ReadandWriteApp
+{
+    public partial class Form1 : Form
+    {
+        private bool status_receiving = false;
+        private Thread thread_print;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (port_printer.IsOpen)
+                {
+                    port_printer.Close();
+                }
+
+                //Setting the Serial Port parameter
+                port_printer.PortName = "COM3";
+                port_printer.BaudRate = 4800;
+                port_printer.DataBits = 8;
+                port_printer.StopBits = StopBits.One;
+
+                //set the read/write timeouts
+                port_printer.ReadTimeout = 500;
+                port_printer.WriteTimeout = 500;
+
+                if (!port_printer.IsOpen)
+                {
+                    //Open the port
+                    port_printer.Open();
+
+                    status_receiving = true;
+
+                    thread_print = new Thread(ReceiveData);
+                    thread_print.IsBackground = true;
+                    thread_print.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        private void ReceiveData()
+        {
+            Byte[] buffer_receive = new Byte[1024];
+
+            try
+            {
+                while (status_receiving)
+                {
+                     if (port_printer.BytesToRead > 0)
+                     {
+                         Int32 length = port_printer.Read(buffer_receive, 0, buffer_receive.Length);
+
+                         string buffer_temp = Encoding.ASCII.GetString(buffer_receive);
+
+                         Array.Resize(ref buffer_receive, length);
+                         Console.WriteLine(buffer_temp); 
+                        //Display d = new Display(ConsoleShow);
+                         //this.Invoke(d, new Object[] { buf });
+                         //Array.Resize(ref buffer, 1024);
+                     }
+                    
+                    Thread.Sleep(20);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SendData(Object buffer_send)
+        {
+            Byte[] buffer_temp = new Byte[1024];
+
+            
+            if (buffer_send != null)
+            {
+                buffer_temp = buffer_send as Byte[];
+
+                try
+                {
+                    port_printer.Write(buffer_temp, 0, buffer_temp.Length);
+                    Console.WriteLine(buffer_temp.ToString());
+                }
+                catch (Exception ex)
+                {
+                    ClosePort();
+                    Console.WriteLine("SENDFAILED");
+                }
+            }
+            else
+            {
+                Console.WriteLine("SENDBUFFER IS NILL");
+            }
+        }
+
+        public void ClosePort()
+        {
+            try
+            {
+                port_printer.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get all ports on computer
+            string[] AllPorts = SerialPort.GetPortNames();
+            txtbx_portlist.Text += "The following serial ports were found"+ Environment.NewLine;
+            foreach (string port in AllPorts)
+            {
+                txtbx_portlist.Text += port;
+                txtbx_portlist.Text += Environment.NewLine;
+            }
+
+        }
+
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+            if (!port_printer.IsOpen)
+            {
+                //Setting the Serial Port parameter
+                port_printer.PortName = "COM3";
+                port_printer.BaudRate = 4800;
+                port_printer.DataBits = 8;
+                port_printer.StopBits = StopBits.One;
+
+                //Open the port
+                port_printer.Open();
+
+                status_receiving = true;
+                byte[] hex_ini = { 0x1B, 0x1B, 0x52, 0x0D };
+                byte[] hex_paper = { 0x1B, 0x1B, 0x4C, 0x52, 0x31, 0x0D };
+                byte[] hex = { 0x1B, 0x1B, 0x50, 0x42, 0x31 };
+                port_printer.Write(hex_ini, 0, hex_ini.Length);
+                port_printer.Write(hex_paper, 0, hex_paper.Length);
+                port_printer.Write(hex, 0, hex.Length);
+            
+            }
+            
+        
+        }
+    }
+}
+
