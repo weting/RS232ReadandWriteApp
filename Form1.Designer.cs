e02ee10b15f9d75754b7a4c01663905bcfd9f3e4
韋廷 呂 <daniel1002135@gmail.com>
2022-10-23 18:11:14 +0800

    2022_1023

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,11 +34,22 @@ namespace RS232ReadandWriteApp
             this.btn_connect = new System.Windows.Forms.Button();
             this.txtbx_portlist = new System.Windows.Forms.TextBox();
             this.btn_print = new System.Windows.Forms.Button();
+            this.txtbx_ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbx_list = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // port_printer
             // 
-            this.port_printer.BaudRate = 300;
             this.port_printer.Handshake = System.IO.Ports.Handshake.RequestToSend;
             this.port_printer.PortName = "COM3";
             this.port_printer.ReadTimeout = 500;
@@ -47,7 +58,7 @@ namespace RS232ReadandWriteApp
             // 
             // btn_connect
             // 
-            this.btn_connect.Location = new System.Drawing.Point(31, 26);
+            this.btn_connect.Location = new System.Drawing.Point(495, 370);
             this.btn_connect.Name = "btn_connect";
             this.btn_connect.Size = new System.Drawing.Size(85, 39);
             this.btn_connect.TabIndex = 0;
@@ -57,7 +68,7 @@ namespace RS232ReadandWriteApp
             // 
             // txtbx_portlist
             // 
-            this.txtbx_portlist.Location = new System.Drawing.Point(361, 71);
+            this.txtbx_portlist.Location = new System.Drawing.Point(495, 94);
             this.txtbx_portlist.Multiline = true;
             this.txtbx_portlist.Name = "txtbx_portlist";
             this.txtbx_portlist.ReadOnly = true;
@@ -66,7 +77,7 @@ namespace RS232ReadandWriteApp
             // 
             // btn_print
             // 
-            this.btn_print.Location = new System.Drawing.Point(31, 97);
+            this.btn_print.Location = new System.Drawing.Point(276, 383);
             this.btn_print.Name = "btn_print";
             this.btn_print.Size = new System.Drawing.Size(85, 41);
             this.btn_print.TabIndex = 2;
@@ -74,17 +85,126 @@ namespace RS232ReadandWriteApp
             this.btn_print.UseVisualStyleBackColor = true;
             this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
             // 
+            // txtbx_
+            // 
+            this.txtbx_.Location = new System.Drawing.Point(49, 28);
+            this.txtbx_.Name = "txtbx_";
+            this.txtbx_.ReadOnly = true;
+            this.txtbx_.Size = new System.Drawing.Size(123, 25);
+            this.txtbx_.TabIndex = 3;
+            this.txtbx_.Text = "阿立圓山運轉所";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "店面";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "地址";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(270, 25);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "台北市大同區承德路三段225巷18號3樓";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "電話";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(94, 25);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "02-2592-9000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "日期時間";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(94, 25);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "細項";
+            // 
+            // txtbx_list
+            // 
+            this.txtbx_list.Location = new System.Drawing.Point(9, 192);
+            this.txtbx_list.Multiline = true;
+            this.txtbx_list.Name = "txtbx_list";
+            this.txtbx_list.Size = new System.Drawing.Size(324, 144);
+            this.txtbx_list.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtbx_);
+            this.groupBox1.Controls.Add(this.txtbx_list);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(28, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 355);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(385, 441);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_print);
             this.Controls.Add(this.txtbx_portlist);
             this.Controls.Add(this.btn_connect);
             this.Name = "Form1";
-            this.Text = "RS232 Printer";
+            this.Text = "TP-7688 Printer";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,6 +216,17 @@ namespace RS232ReadandWriteApp
         private System.Windows.Forms.Button btn_connect;
         private System.Windows.Forms.TextBox txtbx_portlist;
         private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.TextBox txtbx_;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbx_list;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -138,14 +138,17 @@ namespace RS232ReadandWriteApp
 
         private void btn_print_Click(object sender, EventArgs e)
         {
+           // port_printer = new SerialPort("COM3", 4800, Parity.None, 8, StopBits.One);
             if (!port_printer.IsOpen)
             {
                 //Setting the Serial Port parameter
                 port_printer.PortName = "COM3";
-                port_printer.BaudRate = 300;
+                port_printer.BaudRate = 9600;
                 port_printer.DataBits = 8;
                 port_printer.StopBits = StopBits.One;
 
+                port_printer.Encoding = Encoding.Default;
+                Console.WriteLine(port_printer.Encoding.EncodingName.ToString());
                 //set the read/write timeouts
                 port_printer.ReadTimeout = 500;
                 port_printer.WriteTimeout = 500;
@@ -154,21 +157,46 @@ namespace RS232ReadandWriteApp
                 port_printer.Open();
 
                 status_receiving = true;
+                Console.WriteLine(port_printer.IsOpen.ToString());
                 try
                 {
-                    byte[] hex_ini = { 0x1B, 0x1B, 0x52, 0x0D };
-                    byte[] hex_paper = { 0x1B, 0x1B, 0x4C, 0x42, 0x39, 0x0D };
-                    byte[] hex = { 0x1B, 0x1B, 0x50, 0x42, 0x31 };
-                    port_printer.Write(hex_ini, 0, hex_ini.Length);
-                    port_printer.Write(hex_paper, 0, hex_paper.Length);
-                    port_printer.Write(hex, 0, hex.Length);
+                    byte[] hex_askstatus = new byte[]{0x1B, 0x1B, 0x1B, 0x4F, 0x0D };
+                    byte[] hex_ini = new byte[] { 0x1B, 0x1B, 0x52, 0x0D };
+                    byte[] hex_paper = new byte[] { 0x1B, 0x1B, 0x4C, 0x42, 0x39, 0x0D };//送紙測試OK
+                    byte[] hex = new byte[] { 0x1B, 0x1B, 0x50, 0x42, 0x31 };
+                    byte[] hex_print = new byte[] {0x1B, 0x1B, 0x50, 0x52, 0x31, 0x32, 0x33, 0x20, 0x0E, 0x31, 0x0E, 0x32, 0x0E, 0x0D};
+                    byte[] hex_printtest_1 = new byte[] { 0x1B, 0x50, 0x42, 0x31, 0x0B, 0xA4, 0xA4, 0xA4, 0xE5, 0x0B, 0xA4, 0xA4, 0xA4, 0xE5, 0x0D };
+                    byte[] hex_printtest_2 = new byte[] { 0x1B, 0x1B, 0x54, 0x31, 0x20, 0x20, 0x20, 0x20, 0x0B, 0xB0, 0xD3, 0xA9, 0xB1, 0xA6, 0x57, 0xBA, 0xD9, 0x0D };
+                    byte[] hex_printtest_3 = new byte[] { };
+                    byte[] hex_cut = new byte[] { 0x1B, 0x1B, 0x56, 0x52, 0x0D };
 
-                    byte[] dec_ini = { 27, 27, 82, 13 };
-                    byte[] dec_paper = { 27, 27, 76, 68, 57, 13 };
-                    byte[] dec = { };
-                    port_printer.Write(dec_ini, 0, dec_ini.Length);
-                    port_printer.Write(dec_paper, 0, dec_paper.Length);
-                }
+                    //port_printer.Write(hex_askstatus, 0, hex_askstatus.Length);
+                    //port_printer.Write(hex_ini, 0, hex_ini.Length);
+
+                    //port_printer.Write(hex_paper, 0, hex_paper.Length);//TESTOK
+                    //  port_printer.Write(hex, 0, hex.Length);
+                    //port_printer.Write(hex_print, 0, hex_print.Length);
+                    //port_printer.Write(hex_printtest_1, 0, hex_printtest_1.Length);
+                    //port_printer.Write(hex_printtest_2, 0, hex_printtest_2.Length);
+                    //Thread.Sleep(500);
+
+                    port_printer.Write(hex_cut, 0, hex_cut.Length);
+                    Thread.Sleep(500);
+
+                    byte[] dec_ini = new byte[] { 27, 27, 82, 13 };
+                    byte[] dec_paper = new byte[] { 27, 27, 76, 68, 57, 13 };
+                    byte[] dec = new byte[] { };
+                    //  port_printer.Write(dec_ini, 0, dec_ini.Length);
+                    //  port_printer.Write(dec_paper, 0, dec_paper.Length);
+
+                    /*string[] str_paper = new string[] { "ESC", "ESC", "L", "B", "1", "CR" };
+                    for(int i=0; i<str_paper.Length; i++) 
+                    {
+                        port_printer.Write(str_paper[i]);
+                    }*/
+                    Console.WriteLine("Send OK!");
+                    port_printer.Close();
+                 }
                 catch(Exception ex)
                 {
                     MessageBox.Show(ex.ToString());
